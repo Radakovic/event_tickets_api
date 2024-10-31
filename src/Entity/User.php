2d71ba8ad041708bac385467f1af558cd9d05656
rade <radakovicradoslav@gmail.com>
2024-10-31 20:56:42 +0100

    create User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    public function __construct(
+        #[ORM\Column]
+        private string $firstName,
+        #[ORM\Column]
+        private string $lastName,
+        #[ORM\Column(length: 180)]
+        private string $email,
+        #[ORM\Column]
+        private array $roles,
+        #[ORM\Column]
+        private string $password,
+        #[ORM\Id]
+        #[ORM\Column(type: 'uuid', unique: true)]
+        private ?UuidInterface $id = null,
+    ) {
+        $this->id = $id ?? Uuid::uuid4();
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+}
