279494b117d7d2c5def5c8a4c601d71793e3ff36
rade <radakovicradoslav@gmail.com>
2024-11-01 03:17:26 +0100

    Crud operations on Organizer entity

--- src/Entity/Organizer.php
+++ src/Entity/Organizer.php
@@ -2,6 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Entity\Traits\CreateAndUpdatedAtTrait;
 use App\Repository\OrganizerRepository;
 use DateTimeImmutable;
@@ -10,21 +15,36 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: OrganizerRepository::class)]
 #[Gedmo\SoftDeleteable(fieldName: 'deletedAt', hardDelete: false)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Patch(),
+        new Post(),
+    ],
+    normalizationContext: ['groups' => ['get_organizer']],
+    denormalizationContext: ['groups' => ['write_organizer']],
+    security: "is_granted('ROLE_ADMIN')",
+)]
 class Organizer
 {
     use CreateAndUpdatedAtTrait;
 
     public function __construct(
         #[ORM\Column(length: 255)]
+        #[Groups(['get_organizer', 'write_organizer'])]
         private string $name,
 
         #[ORM\Column(length: 255)]
+        #[Groups(['get_organizer', 'write_organizer'])]
         private string $city,
 
         #[ORM\Column(length: 255)]
+        #[Groups(['get_organizer', 'write_organizer'])]
         private string $address,
 
         #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
