6c2a63276ca50af9f9cefc53f2107109eb3692cc
rade <radakovicradoslav@gmail.com>
2024-11-10 19:56:32 +0100

    JWT token

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $payload = $event->getData();
+        $user = $event->getUser();
+        $payload['user'] = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+        ];
+        $event->setData($payload);
+    }
+}
